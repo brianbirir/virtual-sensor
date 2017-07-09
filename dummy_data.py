697c994875351790ec30a3cafef9c2e51aaed809
Brian Birir <brianbirir@gmail.com>
2017-07-09 10:04:52 +0300

    Add dummy data generation function

---
+++ dummy_data.py
@@ -0,0 +1,10 @@
+# Generate random temperature and humidity values
+
+from random import randint
+
+def dummy_temp():
+    return randint(18,30)
+
+
+def dummy_humidity():
+    return randint(56,75)
