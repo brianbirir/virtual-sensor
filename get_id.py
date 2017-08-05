0b7083358757c5995e4022c44b0d18885ec687e5
Brian Birir <brianbirir@gmail.com>
2017-08-05 13:25:51 +0300

    Add spacing

--- get_id.py
+++ get_id.py
@@ -1,10 +1,10 @@
 # Get MAC Address from host computer and use it as an ID
 # Make use of Python UUID
-
 from uuid import getnode as get_mac
 
 
 def get_mac_address():
+
     mac = get_mac()
 
     mac_address = ':'.join(("%012X" % mac)[i:i+2] for i in range(0, 12, 2))
